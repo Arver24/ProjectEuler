606e82a56a7e21d35a452aa4be92d17eaf29e304
Arver24 <arver2405@gmail.com>
2020-05-16 18:04:56 +0530

    Added level 12

---
+++ TriangularDivisible.py
@@ -0,0 +1,23 @@
+n = 200000
+c = 0
+tn = 0
+end = 0
+stop = n
+for i in range(1, stop):
+    c = 0
+    tn = (i * (i + 1)) / 2
+    if (tn ** (1 / 2)) == int(tn ** (1 / 2)):
+        end = int(tn ** (1 / 2))
+    else:
+        end = int(tn ** (1 / 2)) + 1
+    for j in range(1, end):
+        if tn % j == 0:
+            c += 1
+
+    if ((c * 2) + 1) > 500:
+        break
+
+
+print(tn, n, i, stop)
+
+# print(end)
